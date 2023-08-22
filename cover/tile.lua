bb8a0614c6a7ec27c0f918cf0f22dcef694c45de
Florian Wesch <fw@dividuum.de>
2023-08-22 15:04:08 +0200

    simple black cover. Add to layout or page, then control by sending 'root/plugin/cover/alpha:1' or ':0'.

---
+++ cover/tile.lua
@@ -0,0 +1,39 @@
+local api, CHILDS, CONTENTS, PATH = ...
+
+local M = {}
+
+local cover = resource.create_colored_texture(0,0,0,1)
+local easing = require "easing"
+
+local function Transition(from, to, duration)
+    local starts = sys.now()
+    local ends = starts + duration
+
+    return function()
+        local offset = sys.now() - starts
+        local progress = 1.0 / duration * offset
+        progress = math.max(0, math.min(1, progress))
+        local eased = easing.inOutQuad(progress, 0, 1, 1)
+        return from + eased * (to - from)
+    end;
+end
+
+local alpha = Transition(0, 0, 1)
+
+function M.data_trigger(path, data)
+    if path == "alpha" then
+        local target = tonumber(data)
+        alpha = Transition(alpha(), target, 1)
+        print("alpha set to", target)
+    end
+end
+
+function M.task(starts, ends, config, x1, y1, x2, y2)
+    for now in api.frame_between(starts, ends) do
+        if alpha() > 0 then
+            cover:draw(x1, y1, x2, y2, alpha())
+        end
+    end
+end
+
+return M
